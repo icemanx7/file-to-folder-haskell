895360004a6534b43e581c25dbcbd4691d514d60
icemanx7 <drinformations@gmail.com>
2017-08-10 19:14:04 +0200

    added type annotations

--- Main.hs
+++ Main.hs
@@ -8,18 +8,25 @@ import System.FilePath.Posix
 basePath :: String
 basePath = "/tmp/Files"
 
+filePaths :: IO [FilePath]
 filePaths = listDirectory basePath
 
+getBase :: [FilePath] -> [String]
 getBase files = map takeBaseName files
 
+mkDir :: String -> String
 mkDir file = basePath ++ "/" ++ file
 
+getNewPath :: [FilePath] -> [String] -> [FilePath]
 getNewPath f b = map (\(x, y) -> replaceDirectory x y) $ zip f b
 
+createNewDir :: [FilePath] -> IO [()]
 createNewDir a = mapM createDirectory a
 
+moveFiles :: [FilePath] -> [FilePath] -> IO ()
 moveFiles f a = mapM_ (\(x, y) -> renameFile x y) $ zip f a
 
+bearFiles :: IO (IO ())
 bearFiles = do
   file <- filePaths
   let base = getBase file
