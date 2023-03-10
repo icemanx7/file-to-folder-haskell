1dd7fe444f67bd077db1e3aec8b8ef2266fc7846
Richard Thomas <richard.thomas@clearscore.com>
2023-03-10 22:38:05 +0200

    removed stack and refactored the code

--- Main.hs
+++ Main.hs
@@ -2,50 +2,69 @@
 
 module Main where
 
+import Control.Monad (join)
 import Data.Maybe
 import System.Directory
+  ( createDirectory,
+    listDirectory,
+    renameFile,
+  )
+import System.Environment
 import System.FilePath.Posix
 
-rmdups
-  :: Eq a
-  => [a] -> [a]
+rmdups ::
+  Eq a =>
+  [a] ->
+  [a]
 rmdups [] = []
-rmdups (x:xs)
+rmdups (x : xs)
   | x `elem` xs = rmdups xs
   | otherwise = x : rmdups xs
 
-basePath :: String
-basePath = "/run/media/icemanx7/Media/MoviesUnfiltered/"
+data ArgResult = Error | Result String
 
-filePaths :: IO [FilePath]
-filePaths = listDirectory basePath
+filePaths :: String -> IO [FilePath]
+filePaths = listDirectory
 
 getBase :: [FilePath] -> [String]
-getBase files = map takeBaseName files
+getBase = map takeBaseName
 
-mkDir :: String -> String
-mkDir file = basePath ++ "/" ++ file
+mkDir :: String -> String -> String
+mkDir baseP file = baseP ++ "/" ++ file
 
 getNewPath :: [FilePath] -> [String] -> [FilePath]
-getNewPath f b = zipWith replaceDirectory f b
+getNewPath = zipWith replaceDirectory
 
-createNewDir :: [FilePath] -> IO [()]
-createNewDir a = mapM createDirectory a
+createNewDir :: [FilePath] -> IO ()
+createNewDir = mapM_ createDirectory
 
 moveFiles :: [FilePath] -> [FilePath] -> IO ()
-moveFiles f a = mapM_ (\(x, y) -> renameFile x y) $ zip f a
+moveFiles f a = mapM_ (uncurry renameFile) $ zip f a
 
-bearFiles :: IO (IO ())
-bearFiles = do
-  file <- filePaths
+handleInvalidArguments :: Maybe String -> ArgResult
+handleInvalidArguments = maybe Error Result
+
+getSourceDirectory :: IO (Maybe String)
+getSourceDirectory = do
+  arg <- getArgs
+  let s = listToMaybe arg
+  pure s
+
+bearFiles :: ArgResult -> IO ()
+bearFiles (Result str) = do
+  file <- filePaths str
+  let mkDirF = mkDir str
   let base = getBase file
-  let newDir = map mkDir base
-  let old = map mkDir file
-  let newW = (rmdups newDir)
+  let newDir = map mkDirF base
+  let old = map mkDirF file
+  let newW = rmdups newDir
   r <- createNewDir newW
   let final = getNewPath file newDir
-  return (moveFiles old final)
+  join return (moveFiles old final)
+bearFiles Error = putStrLn "Broken"
 
+main :: IO ()
 main = do
-  final <- bearFiles
-  final
+  src <- getSourceDirectory
+  let args = handleInvalidArguments src
+  bearFiles args
